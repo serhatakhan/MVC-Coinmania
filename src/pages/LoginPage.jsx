675fec265ad57deda315297c5a8f9007c3663ea4
serhatakhan <serhat.akhan@outlook.com>
2024-03-12 15:09:44 +0300

    feature(login): login page completed

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,29 +5,32 @@ const LoginPage = () => {
   // formik'in bütün özelliklerini kullanmamızı sağlayan hook
   const formik = useFormik({
     // formda tutulacak verilerin ilk değeri
-    initialValues:{
-        email: "",
-        age: "",
-        password: "",
-        passwordConfirm: ""
+    initialValues: {
+      email: "",
+      age: "",
+      password: "",
+      passwordConfirm: "",
     },
     // Hata yönetimi için,
     // * Validasyon Şeması tanımlamalıyız.
     // * inputlardaki verileri, tanımladığımız şemadaki koşullara uygun mu diye kontrol eder
-    // eğer geçerli değilse error state'ine hataları ekler. 
+    // eğer geçerli değilse error state'ine hataları ekler.
     validationSchema: schema,
 
     // form gönderilince çalışacak fonksiyon,
     // Otomatik olarak sayfa yenilemeyi engeller.
     // 1.parametre(values), formdaki verileri alır.
-    // 2.parametre(actions), formda çalışabilecek aksiyonları alır.(örn reset aksiyonu. sıfırlar) 
-    onSubmit: (values, actions) =>{
-        console.log(values)
-        console.log(actions)
-    }
+    // 2.parametre(actions), formda çalışabilecek aksiyonları alır.(örn reset aksiyonu. sıfırlar)
+    onSubmit: (values, actions) => {
+      console.log(values);
+      console.log(actions);
+    },
   });
 
-  console.log(formik.errors)
+  // erişimi kolay olsun diye formik'in içindeki hataları err'e attık
+  const err = formik.errors;
+  // onBlur olayında, formik'in kendi içinde tuttğu hata yönetim state'i diyebiliriz. formik'i konsola yazdığımzda görebiliyoruz
+  const touched = formik.touched;
 
   return (
     <div className="container-login">
@@ -39,30 +42,69 @@ const LoginPage = () => {
 
         <form onSubmit={formik.handleSubmit}>
           <div className="inputBox1">
-            <input onChange={formik.handleChange} name="email" type="email" required />
+            {/* inputtan odak kaybetme anını izliyoruz onBlur ile. dışarı veya başka bir yere tıklanınca yani */}
+            <input
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              name="email"
+              type="email"
+              required
+              className={`${err.email && touched.email ? "is-invalid" : ""}`}
+            />
+            {/* eğer hata varsa ve o inputa dokunduysak hatayı göster demiş olduk(is-invalid, bootstrapin classı) */}
             <label className="user">Email</label>
+            <div className="feedback">{err?.email} &nbsp; </div>
           </div>
 
           <div className="inputBox">
-            <input onChange={formik.handleChange} name="age" type="number" required />
+            <input
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              name="age"
+              type="number"
+              required
+              className={`${err.age && touched.age ? "is-invalid" : ""}`}
+            />
             <label>Yaş</label>
+            <div className="feedback">{err?.age} &nbsp;</div>
           </div>
 
           <div className="inputBox">
-            <input onChange={formik.handleChange} name="password" type="text" required />
+            <input
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              name="password"
+              type="text"
+              required
+              className={`${err.password && touched.password ? "is-invalid" : ""}`}
+            />
             <label>Şifre</label>
+            <div className="feedback">{err?.password} &nbsp;</div>
           </div>
 
           <div className="inputBox1">
-            <input onChange={formik.handleChange} name="passwordConfirm" type="text" required />
+            <input
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              name="passwordConfirm"
+              type="text"
+              required
+              className={`${err.passwordConfirm && touched.passwordConfirm ? "is-invalid" : ""}`}
+            />
             <label>Şifre Onay</label>
+            <div className="feedback">{err?.passwordConfirm} &nbsp;</div>
           </div>
 
-          <button type="submit" className="btn enter">Gönder</button>
+          <button type="submit" className="btn enter">
+            Gönder
+          </button>
         </form>
       </div>
     </div>
   );
 };
+
+// css ayarlamalarını yaptıktan sonra &nbsp; ie bir boşluk koyduk ki uyarı mesajı çıktığında
+// kayma olmasın !!
 
 export default LoginPage;
--- src/schema.js
+++ src/schema.js
@@ -1,20 +1,48 @@
 // yup'taki bütün fonksiyonları import et,
 // '*' her şeyi yup'tan import et demek, 'as yup' ise import ettiklerini yup olarak tanımla demek
-import * as yup from "yup"
+import * as yup from "yup";
 
 // validasyon şeması
 // görevi, formdaki inputların geçerli olması için gerekli koşulları tanımladığımız alan.
 
+// 1 büyük harf, bunların hepsi minimum tabi
+// 1 küçük harf
+// 1 sayı
+// 1 özel karakter
+// min 5 karakter
+const regex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{5,}$";
+
 // yup.object().shape --> burası kalıp yapı
 // * bir alan için koşulları yazarken ilk olarak o alanın tipini tanımlayan
-// yup fonksiyonunu çağırırız 
+// yup fonksiyonunu çağırırız
 export const schema = yup.object().shape({
-    // email'in geçerli olması için koşullar
-    email: yup.string().email().required(),
-    // yaşın geçerli olması için koşullar, integer()->tam sayı
-    age: yup.number().min(18).max(100).integer().required(),
-})
+  // email'in geçerli olması için koşullar
+  email: yup
+    .string()
+    .email("Email geçerli bir formatta olmalıdır")
+    .required("Email zorunlu bir alandır"),
 
+  // yaşın geçerli olması için koşullar, integer()->tam sayı
+  age: yup
+    .number()
+    .min(18, "Yaş 18'den küçük olamaz")
+    .max(100, "Yaş 100'den büyük olamaz")
+    .integer("Yaşınızı tam sayı olarak giriniz")
+    .required("Yaş zorunlu bir alandır"),
 
+  // şifrenin geçerli olması için koşullar
+  password: yup
+    .string()
+    .min(5, "Şifreniz en az 5 karakter içermeli")
+    // şifre, regex kurallarına uygun mu kontrol eder matches
+    .matches(regex, "Şifreniz yeterince güçlü değil")
+    .required("Şifre zorunlu bir alandır"),
 
-
+  // şifre onay alanının geçerli olması için koşullar
+  passwordConfirm: yup
+  .string()
+  // oneOf(), kontrol ettiğimiz inputtaki verinin verdiğimiz değerlere eşit olup olmadığını kontrol eder,
+  // ref(), farklı bir inputtaki veriye erişmemizi sağlar.
+  .oneOf([yup.ref("password")], "Şifreniz eşleşmiyor")
+  .required("Lütfen şifrenizi onaylayın"),
+});
